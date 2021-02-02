311c80aa8b7c27ad624c2cf702745843b4dfe174
piniaczchemik <72197411+piniaczchemik@users.noreply.github.com>
2021-02-02 14:38:43 +0000

    Created first classes

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,13 +1,43 @@
 ﻿namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This class offers methods for converting a given
+    /// distance measured in miles to equivalent 
+    /// distance measured in feet
     /// </summary>
     /// <author>
     /// Student Name version 0.1
-    /// </author>
-    public class DistanceConverter
+    /// Kamil Lesniewski 21906354
+    public class DistanceConverter 
     {
+    }
+        private double miles;
+        private double feet;
+            
+            /// <summary>
+            /// 
+            /// </summary>
+        public void Run() 
+        {
+        
+        }
+        /// <summary>
+        /// Prompt the user to enter the distance in miles
+        /// Input the miles as a double number.
+        /// </summary>
+        private void InputMiles() 
+        {
+        
+        }
 
+        private void CalculateFeet() 
+        {
+        
+        }
+
+        private void OutputFeet() 
+        {
+        
+        }
     }
 }
