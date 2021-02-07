7031110dd8085c8c8c630c07d1b555c33fcdd87f
piniaczchemik <72197411+piniaczchemik@users.noreply.github.com>
2021-02-07 20:09:40 +0000

    Added new methods to the converter

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -5,38 +5,61 @@ namespace ConsoleAppProject.App01
     /// <summary>
     /// This class offers methods for converting a given
     /// distance measured in miles to equivalent 
-    /// distance measured in feet
+    /// distance measured in feet.
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Student Name version 0.3
     /// Kamil Lesniewski 21906354
     public class DistanceConverter 
     {
         public const int FEET_IN_MILES = 5280;
+
+        public const double METRES_IN_MILES = 1609.34;
         
         private double miles;
 
         private double feet;
 
+        private double metres;
+
        /// <summary>
-       /// 
+       /// This method will input the distance measured in miles
+       /// calculate the same distance in feet, and output the 
+       /// distance in feet.
        /// </summary>
+       
+        public void MilesToFeet()
+        {
+            OutputHeading("Converting Miles To Feet");
+
+            InputMiles();
+            CalculateFeet();
+            OutputFeet();
+        }
+
+        public void FeetToMiles()
+        {
+            OutputHeading("Converting Feet to Miles");
+
+            Inputfeet();
+            CalculateMiles();
+            OutputMiles();
+        }
+
+        public void MilesToMetres()
+        {
+            OutputHeading("Converting Miles to Metres");
+
+            InputMiles();
+            CalculateMetres();
+            OutputMetres();
+        }
         public void Run() 
         {
-            OutputHeading();
             InputMiles();
-            Calculatefeet();
-            outputFeet();
+            CalculateFeet();
+            OutputFeet();
         }
-
-        private void OutputHeading()
-        {
-            Console.WriteLine("\n-----------------------------------");
-            Console.WriteLine("       Convert Miles to Feet         ");
-            Console.WriteLine("        By Kamil Lesniewski          ");
-            Console.WriteLine("-----------------------------------\n");
-        }
-
         /// <summary>
         /// Prompt the user to enter the distance in miles
         /// Input the miles as a double number
@@ -47,15 +70,85 @@ namespace ConsoleAppProject.App01
             string value = Console.ReadLine();
             miles = Convert.ToDouble(value);
         }
-        private void Calculatefeet() 
+
+        private void InputMetres()
         {
-            feet = miles * 5280;
-        }
-    
-        private void outputFeet() 
+            Console.Write("Please enter the number of metres > ");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
+         }
+        private void Inputfeet()
         {
-            Console.WriteLine(miles + " miles is " + feet + " feet! ");
+            Console.Write("Please enter the number of feet > ");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+
+        private void CalculateMiles()
+        {
+            miles = feet / FEET_IN_MILES;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        
+        private void CalculateFeet()
+        {
+            feet = miles * FEET_IN_MILES;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        
+        private void CalculateMetres()
+        {
+            metres = miles * METRES_IN_MILES;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// 
+
+        private void OutputFeet()
+        {
+            Console.WriteLine(miles + " miles is " + feet + " feet!");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        
+        private void OutputMiles() 
+        {
+            Console.WriteLine(feet + " feet is " + miles + " miles!");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// 
+
+        private void OutputMetres()
+        {
+            Console.WriteLine(miles + " miles is " + metres + " metres!");
+        }
+
+        private void OutputHeading(String prompt)
+        {
+            Console.WriteLine("\n-----------------------------------");
+            Console.WriteLine("         Distance Converter        ");
+            Console.WriteLine("         By Kamil Lesniewski          ");
+            Console.WriteLine("-----------------------------------\n");
+
+            Console.WriteLine("Prompt");
+            Console.WriteLine();
+        }
     }
 }
--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -22,7 +22,10 @@ namespace ConsoleAppProject
             Console.Beep();
 
             DistanceConverter converter = new DistanceConverter();
-            converter.Run();
+            converter.MilesToFeet();
+            converter.FeetToMiles();
+            converter.MilesToMetres();
+
         }
     }
 }
